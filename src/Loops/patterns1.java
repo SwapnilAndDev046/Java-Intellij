62d03b40df18c4cbb5d1bbc6e163dd5f5fc38cad
SwapnilAndDev046 <swapnildevkategmi@gmail.com>
2024-12-21 20:51:32 +0530

    commit

--- src/Loops/patterns1.java
+++ src/Loops/patterns1.java
@@ -1,11 +1,16 @@
 package Loops;
-//*
-//* *
-//* * *
-//* * * *
-//* * * * *
+//i=1*
+//i=2* *
+//i=3* * *
+//i=4* * * *
+//i=5* * * * *
 public class patterns1 {
     public static void main(String[] args) {
-
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
